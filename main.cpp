d3b2156778a3ef5317fc15a9d9467f2197c0c976
Geoffrey Cline <geoffreyc@outlook.com>
2015-10-01 00:51:58 -0500

    inital commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cs3800_hw1
+//
+//  Created by Geoffrey on 10/1/15.
+//  Copyright (c) 2015 Geoffrey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
